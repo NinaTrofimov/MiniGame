e65e6bf6add33c5138e125f7a6b863a017747962
NinaTrofimov <ntrofimov1@my.waketech.edu>
2025-03-04 19:23:54 -0500

    updated logic

---
+++ items.py
@@ -0,0 +1,38 @@
+class Items(object):
+    
+    def __init__(self, capacity):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __getitem__(self,index):
+        pass
+
+    def __setitem__(self,newItem):
+        pass
+
+    def size(self):
+        pass
+
+    def __grow(self):
+        pass
+
+    def __shrink(self):
+        pass
+
+    def insert(self,index,newItem):
+        pass
+
+    def remove(self):
+        pass
+
+
+def item_list():
+    pass
--- main.py
+++ main.py
@@ -1,19 +1,61 @@
 import pickle
 from dataclasses import dataclass
-
+import random
 
 def main():
     @dataclass
     class Character:
-        name: str
-        age: str
-        type: str
+        def __init__(self,name,age,region):
+            self.name = name
+            self.age = age
+            self.region = region
+        
+        def add_attack(self,attack):
+            pass
 
-    class Attack():
-        name : str
-        hp: int
-        attack: int
-        mana: int
+        def add_defense(self,defense):
+            pass
+
+        def add_hp(self,hplist):
+            pass
+
+        def add_items(self,items):
+            pass
+        
+    class WeaponsList:
+        def __init__(self,name,attacklist = None,defenselist = None,raisehp = None,itemlist=None):
+            self.name = name
+            self.attacklist = list()
+            self.defenselist = list()
+            self.raisehp = list()
+            self.itemlist = list()
+        def __weaponsnames__(self):
+            if Character.region == 'wizard':
+                attacks = ['Beam','Staff']
+                defense = ['Barrier']
+                hp = ['Revive']
+                self.attacklist.extend(attacks)
+            elif Character.region == 'knight':
+                self.attacklist = ['Sword']
+                self.defenselist = ['Shield','Block']
+            elif Character.region == 'Solider':
+                self.attacklist = ['Gun','']
+        
+    class AttackAttributes():
+        def __init__(self,name,damage):
+            pass
+        
+    class Action():
+        def __init__(self,name,damage,hp):
+            self.name = name
+            self.damage = damage
+        
+        def attack_use(self,target):
+            crit = random.random() < 0.2 
+            final_damage = self.damage * (2 if crit else 1)
+            crit_text = " (Critical Hit!)" if crit else ""
+            return f"{Character.name} hits {target} for {final_damage} damage!{crit_text}"
+
 
     test = input(" ")
     while test != 'exit':
@@ -39,9 +81,5 @@ def main():
                 char = Character(character_data[0], character_data[1], character_data[2])
                 characters.append(char)
 
-                #work on this
-                Attack(character_data[0])
-                if character_data[1] <= 18:
-                    Attack(character_data[1])
             with open('characters.pkl', 'wb') as file:
                 pickle.dump(characters, file)
