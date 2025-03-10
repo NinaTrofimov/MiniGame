3da948a51d0acf38eebaffb4d09bec5648a80c9a
NinaTrofimov <ntrofimov1@my.waketech.edu>
2025-03-09 20:52:27 -0400

    update main.py

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ import random
 def main():
     @dataclass
     class Character:
-        def __init__(self,name,age,region,hp,mana):
+        def __init__(self,name,age,region,hp,mana,attacks,defense,healing,items):
             self.name = name
             self.age = age
             self.region = region
             self.hp = hp
             self.mana = mana
+            self.attacks = list()
+            self.defense = list()
+            self.healing = list()
+            self.items = list()
         
         def add_attack(self,attack):
-            pass
+            self.attacks.append(attack)
 
         def add_defense(self,defense):
-            pass
+            self.defense.append(defense)
 
         def add_healing(self,healinglist):
-            pass
+            self.healing.append(healinglist)
 
         def add_items(self,items):
-            pass
+            self.items.append(items)
         
     class WeaponsList:
         def __init__(self,name,attacklist = None,defenselist = None, healinglist = None,itemlist=None):
@@ -31,8 +35,8 @@ def main():
             self.defenselist = list()
             self.healinglist = list()
             self.itemlist = list()
-        def __weaponsnames__(self):
-            if Character.region == 'wizard':
+        def weaponsnames(self,region): # Character.weaponsname(region)
+            if region == 'wizard':
                 Beam = CharacterItemAttributes('Beam','Attack',15,10)
                 Staff = CharacterItemAttributes('Staff','Attack',10,5)
 
@@ -44,7 +48,7 @@ def main():
                 self.attacklist.extend(Beam,Staff)
                 self.defenselist.append(Barrier)
                 self.healinglist.extend(Revive,Potion)
-            elif Character.region == 'knight':
+            elif region == 'knight':
                 Sword = CharacterItemAttributes('Sword','Attack', 25,15)
 
                 Shield = CharacterItemAttributes('Shield','Defense',20,10)
@@ -55,14 +59,14 @@ def main():
                 self.attacklist.append(Sword)
                 self.defenselist.extend(Shield,Block)
                 self.healinglist.append(Food)
-            elif Character.region == 'solider':
+            elif region == 'solider':
                 Gun = CharacterItemAttributes('Bullets','Multi-Attack',10,20)
                 Shotgun = CharacterItemAttributes('Shotgun','Attack',20,13)
 
 
 
                 self.attacklist.extend(Gun,Shotgun)
-            elif Character.region == 'ninja':
+            elif region == 'ninja':
                 Shuriken = CharacterItemAttributes('Shuriken','Multi-Attack',10,20)
         
     class CharacterItemAttributes():
